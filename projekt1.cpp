9c6af1b1542cad6cbabccb70da1ad8f1ea9dc8e8
Arek Szewczyk <arek.szewczyk@hotmail.com>
2024-12-27 13:08:46 +0100

    Dodanie silni do menu

--- projekt1.cpp
+++ projekt1.cpp
@@ -19,8 +19,17 @@ int main() {
         cout << endl;
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
+	cout << "1. Silnia" << endl;
         cout << "0. Wyjscie" << endl;
         cin >> wyjscie;
+	if(wyjscie==1)
+	{
+		int liczba;
+		cout<<"Podaj z jakiej liczby wyliczyc silnie"<<endl;
+		cin>>liczba;
+		cout<<"Wynik: " << silnia(liczba)<<endl;
+		return 0;
+	}
     } while(wyjscie != 0);
     return 0;
 }
